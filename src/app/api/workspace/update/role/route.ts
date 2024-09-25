5ecbd513ccee953976e14c6f7d3acebb5d3bb670
Own3r <darkwebsite95@gmail.com>
2024-09-25 20:09:23 +0330

    route for changing user's role added, route for deleting a member from a workspace added

---
+++ src/app/api/workspace/update/role/route.ts
@@ -0,0 +1,89 @@
+import { auth } from "@/auth";
+import db from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request){ 
+    try {
+
+        const user = await auth();
+
+        if (!user) {
+            return NextResponse.json({ success: false, message: "Login is required!" });
+        }
+
+        const data = await req.json();
+
+        const { id, workspaceId } = data;
+
+        if (!workspaceId || !id) {
+            return NextResponse.json({ success: false, message: "workspaceId and MemberId is required!" });
+        }
+
+        const findMember = await db.members.findUnique({
+            where: {
+                workspaceId,
+                id
+            }
+        })
+
+        if (!findMember) {
+            return NextResponse.json({ success: false, message: "Member cannot be found!" });
+        }
+
+        const findAdmin = await db.user.findUnique({
+            where: {
+                id: user.user.id
+            },
+            include: {
+                Workspaces: {
+                    where: {
+                        id: workspaceId
+                    },
+                    // include: {
+                    //     Members: {
+                    //         where: {
+                    //             id: {
+
+                    //             }
+                    //         }
+                    //     }
+                    // }
+                }
+            }
+        })
+
+        if (!findAdmin) {
+            return NextResponse.json({ success: false, message: "Your account cannot be found!" });
+        }
+
+        const checkIsAdmin = await db.members.findFirst({
+            where: {
+                workspaceId,
+                userId: findAdmin.id,
+
+            }
+        })
+
+        if (!checkIsAdmin || checkIsAdmin.role !== "admin") {
+            return NextResponse.json({ success: false, message: "You're not Admin of this Workspace!" });
+        }
+
+        const editRole = await db.members.update({
+            where: {
+                workspaceId: id,
+                id
+            },
+            data: {
+                role: "admin"
+            }
+        })
+
+        if (editRole) {
+            return NextResponse.json({ success: true, message: "User role changed!" });
+        } else {
+            return NextResponse.json({ success: false, message: "Something went Wrong!" });
+        }
+    } catch (error) {
+        return NextResponse.json({ success: false, message: "Something went wrong!" });
+    }
+}
